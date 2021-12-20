3a61749b3908a0484da3ecbe37baee864efbd111
Bruno Edson <brunoedson2201@gmail.com>
2021-12-20 20:49:06 -0300

    Adiona lista de sprites na cena

--- js/Cena.js
+++ js/Cena.js
@@ -3,9 +3,17 @@ export default class Cena { //é responsável por desenhar elementos na tela em 
     constructor(canvas){
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
+        this.sprites = [];
     }
     desenhar(){
         this.ctx.fillStyle = "grey";
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        for (let s = 0; s < this.sprites.length; s++) {
+            const sprite = this.sprites[s];
+            sprite.desenhar(this.ctx);
+        }
+    }
+    adicionar(sprite){
+        this.sprites.push(sprite);
     }
 }
