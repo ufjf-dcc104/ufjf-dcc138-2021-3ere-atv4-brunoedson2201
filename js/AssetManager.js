24260c18cce7a998d527861c9ae2933a63ca1638
Bruno Edson <brunoedson2201@gmail.com>
2021-12-21 23:02:25 -0300

    Adiciona um método acabou()

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -7,8 +7,14 @@ export default class AssetManager {
 
     carregaImagem(chave, source) {
         const img1 = new Image();
+        img1.addEventListener("load", () => {
+            console.log(`Imagem ${this.carregadas}/${this.aCarregar} carregada!`);
+            this.carregadas++;
+        });
+
         img1.src = source;
         this.imagens.set(chave, img1);
+        this.aCarregar++;
     }
 
     img(chave){
@@ -17,8 +23,12 @@ export default class AssetManager {
 
     progresso() {
         if (this.aCarregar > 0) {
-            return `${(this.carregadas / this.aCarregar * 100).toFixed(2)}}%`;
+            return `${(this.carregadas / this.aCarregar * 100).toFixed(2)}%`;
         }
         return "Nada a carregar";
+    }
+
+    acabou(){
+        return this.carregadas === this.aCarregar;
     }
 }
