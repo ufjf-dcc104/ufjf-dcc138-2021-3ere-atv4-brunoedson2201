e55560ad193fb40c103745aeb2059a0b4b3ae15e
Bruno Edson <brunoedson2201@gmail.com>
2021-12-21 22:54:20 -0300

    Refatora load e uso de imagem para o assetManager

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -1,12 +1,23 @@
-export default class AssetManager{
-    constructor(){
+export default class AssetManager {
+    constructor() {
         this.aCarregar = 0;
         this.carregadas = 0;
+        this.imagens = new Map();
     }
 
-    progresso(){
-        if(this.aCarregar > 0){
-            return `${(this.carregadas/this.aCarregar * 100).toFixed(2)}}%`;
+    carregaImagem(chave, source) {
+        const img1 = new Image();
+        img1.src = source;
+        this.imagens.set(chave, img1);
+    }
+
+    img(chave){
+        return this.imagens.get(chave);
+    }
+
+    progresso() {
+        if (this.aCarregar > 0) {
+            return `${(this.carregadas / this.aCarregar * 100).toFixed(2)}}%`;
         }
         return "Nada a carregar";
     }
