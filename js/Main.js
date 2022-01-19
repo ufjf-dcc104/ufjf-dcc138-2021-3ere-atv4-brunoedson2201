f3abc50dbaf27320999393af2fd302cae140897a
Bruno Edson <brunoedson2201@gmail.com>
2022-01-19 20:49:14 -0300

    Cria gerenciador de entrada de controle

--- js/Main.js
+++ js/Main.js
@@ -4,7 +4,9 @@ import AssetManager from "./AssetManager.js";
 import Mapa from "./Mapa.js";
 import { mapa1 as modeloMapa1 } from "../maps/mapa1.js";
 import Mixer from "./Mixer.js";
+import InputManager from "./InputManager.js";
 
+const input = new InputManager();
 const mixer = new Mixer(10);
 const assets = new AssetManager(mixer);
 
@@ -18,6 +20,12 @@ assets.carregaAudio("boom", "assets/boom.wav");
 const canvas = document.querySelector("canvas");
 canvas.width = 14 * 32;
 canvas.height = 10 * 32;
+
+input.configurarTeclado({
+    "ArrowLeft": "MOVE-ESQUERDA",
+    "ArrowRight": "MOVE-DIREITA",
+});
+
 const cena1 = new Cena(canvas, assets);
 
 const mapa1 = new Mapa(10, 14, 32);
