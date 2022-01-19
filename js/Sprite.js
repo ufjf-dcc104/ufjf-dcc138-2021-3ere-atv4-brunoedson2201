2909ef5615a513744550a90140aa3bd6930ae64f
Bruno Edson <brunoedson2201@gmail.com>
2022-01-18 21:31:15 -0300

    Adiciona restrições de mapa em todas direções

--- js/Sprite.js
+++ js/Sprite.js
@@ -39,6 +39,8 @@ export default class Sprite { //responsável por modelar algo que se move na tel
     aplicaRestricoes(dt) {
         this.aplicaRestricoesDireita(dt);
         this.aplicaRestricoesEsquerda(dt);
+        this.aplicaRestricoesBaixo(dt);
+        this.aplicaRestricoesCima(dt);
 
     }
     aplicaRestricoesDireita(dt) {
@@ -79,6 +81,48 @@ export default class Sprite { //responsável por modelar algo que se move na tel
                 if (this.colidiuCom(tile)) {
                     this.vx = 0;
                     this.x = tile.x + tile.w / 2 + this.w / 2 + 1;
+                }
+            }
+        }
+    }
+    aplicaRestricoesBaixo(dt) {
+        if (this.vy > 0) {
+            const SIZE = this.cena.mapa.SIZE;
+            const pmx = this.mx;
+            const pmy = this.my + 1;
+            if (this.cena.mapa.tiles[pmy][pmx] != 0) {
+                const tile = {
+                    x: pmx * SIZE + SIZE / 2,
+                    y: pmy * SIZE + SIZE / 2,
+                    w: SIZE,
+                    h: SIZE
+                }
+                this.cena.ctx.strokeStyle = "white";
+                this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE);
+                if (this.colidiuCom(tile)) {
+                    this.vy = 0;
+                    this.y = tile.y - tile.h / 2 - this.h / 2 - 1;
+                }
+            }
+        }
+    }
+    aplicaRestricoesCima(dt) {
+        if (this.vy < 0) {
+            const SIZE = this.cena.mapa.SIZE;
+            const pmx = this.mx;
+            const pmy = this.my - 1;
+            if (this.cena.mapa.tiles[pmy][pmx] != 0) {
+                const tile = {
+                    x: pmx * SIZE + SIZE / 2,
+                    y: pmy * SIZE + SIZE / 2,
+                    w: SIZE,
+                    h: SIZE
+                }
+                this.cena.ctx.strokeStyle = "white";
+                this.cena.ctx.strokeRect(tile.x - SIZE / 2, tile.y - SIZE / 2, SIZE, SIZE);
+                if (this.colidiuCom(tile)) {
+                    this.vy = 0;
+                    this.y = tile.y + tile.h / 2 + this.h / 2 + 1;
                 }
             }
         }
